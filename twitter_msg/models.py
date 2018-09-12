6179ebabf0485f332985fc2967f8144fca7642da
PieterMobile <pieter.bruegel10@gmail.com>
2018-09-12 10:53:40 -0700

    Update models.py

--- twitter_msg/models.py
+++ twitter_msg/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from datetime import datetime
+from django.utils import timezone
 
 # Create your models here.
 class Message(models.Model):
     message_id = models.AutoField(primary_key=True)
     message_name = models.CharField(max_length=50)
     message_content = models.TextField(max_length=50)
-    message_time = models.DateTimeField(default=datetime.now().replace(microsecond=0))
+    message_time = models.DateTimeField(default=timezone.now)
